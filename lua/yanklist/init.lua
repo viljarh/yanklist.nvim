bb0d1c02ef8656df1676a79cb22e2afcdba9b9af
Viljar Hoem-Olsen <viljar.hoem.olsen@gmail.com>
2025-01-05 23:03:44 +0100

    Added a default keybinding to toggle side panel

--- lua/yanklist/init.lua
+++ lua/yanklist/init.lua
@@ -1,7 +1,22 @@
 local M = {}
 
-function M.setup()
+function M.setup(user_config)
 	require("yanklist.core").initialize()
+
+	vim.api.nvim_set_keymap(
+		"n",
+		"<leader>yl",
+		"<cmd>lua require('yanklist.ui').toggle_side_panel()<CR>",
+		{ noremap = true, silent = true }
+	)
+	if user_config and user_config.keymap then
+		vim.api.nvim_set_keymap(
+			"n",
+			user_config.keymap,
+			"<cmd>lua require('yanklist.ui').toggle_side_panel()<CR>",
+			{ noremap = true, silent = true }
+		)
+	end
 end
 
 return M
